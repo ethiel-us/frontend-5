7eefcc27986bfdab5e3c11a8e022cbbfa1ac216b
Ethiel Blandón <ethblacub@alum.us.es>
2026-05-11 19:41:51 +0200

    Initial commit

---
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/EditProductScreen.js
@@ -0,0 +1,5 @@
+import TextRegular from '../../components/TextRegular'
+
+export default function EditProductScreen({ navigation, route }) {
+  return <TextRegular>Edit Product Screen to be implemented</TextRegular>
+}
